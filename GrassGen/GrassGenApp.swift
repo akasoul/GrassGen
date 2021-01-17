c14d71045464b8ca4b1f8859cfb75c16472320cb
akasoul <antonvoloshuk@gmail.com>
2021-01-17 16:01:45 +0300

    Initial Commit

---
+++ GrassGen/GrassGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrassGenApp.swift
+//  GrassGen
+//
+//  Created by Anton Voloshuk on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct GrassGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
